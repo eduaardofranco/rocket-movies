4adbfa8065ade2624c733b1d38fa79443de0f740
eduaardofranco <eduaardo.franco@gmail.com>
2023-09-25 10:43:29 +0100

    login authenticated

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,12 +2,15 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
+import { useAuth } from '../hooks/auth'
 
 export function Routes() {
+    const { user } = useAuth()
     return(
 
         <BrowserRouter>
-            <AuthRoutes />
+        {/* if is authenticated show <AppRoutes> else show <AuthRoutes> */}
+            {user ? <AppRoutes /> : <AuthRoutes />}
         </BrowserRouter>
 
     )
