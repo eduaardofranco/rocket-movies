4dab490388d189744965811fbba993b65e5e1685
eduaardofranco <eduaardo.franco@gmail.com>
2023-10-06 20:07:11 +0100

    endpoint update

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export const api = axios.create({
     // baseURL: 'https://rocketmovies-api-pyl7.onrender.com'
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: process.env.REACT_APP_API_URL || 'https://rocketmovies-api-pyl7.onrender.com'
     // baseURL: 'http://localhost:3333'
 })
