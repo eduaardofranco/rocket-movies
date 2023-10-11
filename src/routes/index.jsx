fc7d52b86157c1aebb22577cc686a3af87f20bb9
eduaardofranco <eduaardo.franco@gmail.com>
2023-10-11 07:03:23 +0100

    change endpoint

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,21 @@
 import { BrowserRouter } from 'react-router-dom'
-
+import { useEffect } from 'react'
 import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
 import { useAuth } from '../hooks/auth'
+import { api } from '../services/api'
+
 
 export function Routes() {
-    const { user } = useAuth()
+    const { user, signOut } = useAuth()
+
+    useEffect(() => {
+        api.get('/users/validated').catch((error) => {
+            console.log(error)
+            signOut()
+        })
+    },[])
+
     return(
 
         <BrowserRouter>
